2e31d44ed2846d6997b2a48e50d3f0e910b1090b
Evgeny <save.this.is@gmail.com>
2021-02-11 17:31:37 +0500

    Fix model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Question(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
-
 class Answer(models.Model):
     text = models.TextField()
     question = models.ForeignKey('Question')
@@ -22,3 +14,12 @@ class QuestionManager(models.Manager):
 
   def popular(self):
     return self.order_by('-rating')
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
